fc8df1172223bb59634ce29df81189f0b3991e7b
Robson William <robsonwilliamro@gmail.com>
2024-04-01 12:13:23 -0300

    create: CheckboxInput component

---
+++ src/components/Input/CheckboxInput.tsx
@@ -0,0 +1,10 @@
+export function CheckboxInput() {
+    return (
+        <>
+            <div className="custom-checkbox-container">
+                <input type="checkbox" id="hortalica1" value="Alface" className="custom-input-checkbox"/>
+                <label htmlFor="hortalica1">Alface</label>
+            </div>
+        </>
+    )
+}
